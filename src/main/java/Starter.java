1c1867144700b007df96013a248ee34c05939660
yuwantha98 <yuwantharavihara123@gmail.com>
2025-01-15 20:59:34 +0530

    Add main dashboard

---
+++ src/main/java/Starter.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard_form.fxml"))));
+        stage.show();
+        stage.setTitle("Dulmark Clothing");
+    }
+}
---
+++ src/main/java/controller/dashboardFormController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class dashboardFormController {
+
+    @FXML
+    public JFXTextField txtPassword;
+
+    @FXML
+    public JFXTextField txtEmail;
+
+    @FXML
+    public void btnEmpolyeeLogin(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void btnEmpolyeeSignUp(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void btnAdminLogin(ActionEvent actionEvent) {
+    }
+}
